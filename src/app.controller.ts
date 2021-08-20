72d0a7866a8d2f11f1d7f5db7be090677c5638c5
Nick Freitas <nickfrei@gmail.com>
2021-08-19 22:57:29 -0400

    setup test on all services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
